066bf5574e8c6232158b491e68c0c35951a820a0
Damian Hickey <dhickey@gmail.com>
2015-09-26 13:34:11 +0200

    AssemblyInfo files were missing on import

---
+++ build/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft OWIN")]
+[assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.30820.0")]
+[assembly: AssemblyInformationalVersion("3.0.1-rtw-30820-000")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
---
+++ build/DevAssemblyInfo.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft OWIN")]
+[assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.30.0.0")]
+[assembly: AssemblyFileVersion("0.30.30820.0")]
+[assembly: AssemblyInformationalVersion("0.30.0-pre-30820-000")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
